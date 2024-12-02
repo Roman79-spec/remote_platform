380fa41ccd43c4010fdaaea785ebfba5a231f625
Roman <carltokaize2000@mail.ru>
2024-12-03 01:31:00 +0300

    Добавляю аннотации типов

--- remote_platform.py
+++ remote_platform.py
@@ -1,21 +1,21 @@
 class Users:
     """Класс для управления пользователями"""
-    def __init__(self, first_name, last_name, login, password):
+    def __init__(self, first_name: str, last_name: str, login: str, password: str)-> None:
         """Инициализация атрибутов"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.login = login
-        self.password = password
-        self.progress = 0
-        self.personal_course = []
-        self.history_complete_course = []
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+        self.login: str = login
+        self.password: str = password
+        self.progress: int = 0
+        self.personal_course: list = []
+        self.history_complete_course: list = []
     
-    def __str__(self):
+    def __str__(self) -> None:
         """Строковый метод для вывода информации о пользователе"""
         return (f'Имя: {self.first_name}. Фамилия: {self.last_name}.\n'
                 f'Логин: {self.login}. Пароль: {self.password}.')
     
-    def select_course(self, course):
+    def select_course(self, course: 'Courses')-> None:
         """Выбор курса"""
         print(f'\nПопытка выбрать курс: {course.course_name}')
         if course.course_name not in self.personal_course:
@@ -24,13 +24,13 @@ class Users:
         else:
             print('\nНельзя выбрать один и тот же курс!')
     
-    def print_select_courses(self):
+    def print_select_courses(self)-> None:
         """Вывод выбранных курсов"""
         print('\nВыбранные курсы: ')
         for course in self.personal_course:
             print(f'Курс: {course.course_name}. Статус: {course.status}')
 
-    def print_history_complete_courses(self):
+    def print_history_complete_courses(self)-> None:
         """Вывод списка завершенных курсов"""
         if self.history_complete_course:
             print('Завершенные курсы: ')
@@ -39,40 +39,40 @@ class Users:
         else:
             print('Вы еще не прошли курс!')
     
-    def show_progress(self):
+    def show_progress(self)-> None:
         """Вывод прогресса пользователя"""
         print(f'\nПрогресс пользователя {self.first_name} {self.last_name}')
         if not self.personal_course:
             print('Еще ни одного курса не выбрано!')
         for course in self.personal_course:
             print(f'Курс {course.course_name}. Прогресс: {course.progress}.\n'
-                  'Статус {course.status}.')
+                  f'Статус {course.status}.')
 
 class Platform:
     """Для управления и добавление курсов"""
-    def __init__(self):
+    def __init__(self)-> None:
         """Инициализация атрбиутов"""
-        self.all_users = {}
-        self.course_list = {}
+        self.all_users: dict = {}
+        self.course_list: dict = {}
 
-    def user_registrations(self, identifer_user, user):
+    def user_registrations(self, identifer_user: str, user: 'Users')-> None:
         """Регистрация пользователей"""
         if identifer_user not in self.all_users:
             self.all_users[identifer_user] = user
         else:
             print('Такой пользователь уже существует!')
         
-    def print_users(self):
+    def print_users(self)-> None:
         """Вывод все курсов"""
         print('Список всех пользователей: ')
         for user in self.all_users.values():
             print(user)
 
-    def add_course(self, identifier_course, course):
+    def add_course(self, identifier_course: str, course: 'Courses')-> None:
         """Добавление курса на платформу"""
         self.course_list[identifier_course] = course
 
-    def print_courses(self):
+    def print_courses(self)-> None:
         """Вывод всех курсов"""
         if self.course_list:
             print('Список доступных курсов: ')
@@ -83,19 +83,22 @@ class Platform:
             
 class Courses:
     """Для управления курсами"""
-    def __init__(self, course_name, direction_course):
+    NOT_STARTED = 'not started'
+    IN_PROGRESS = 'in progress'
+    COMPLETE = 'complete'
+    def __init__(self, course_name: str, direction_course: str)-> None:
         """Инициализация атрибутов"""
-        self.course_name = course_name
-        self.direction_course = direction_course
-        self.modules = []
-        self.status = 'not started'
-        self.progress = 0
+        self.course_name: str = course_name
+        self.direction_course: str = direction_course
+        self.modules: list = []
+        self.status: str = Courses.NOT_STARTED
+        self.progress: int = 0
 
-    def __str__(self):
+    def __str__(self)-> None:
         """Строковый метод"""
         return f'Название: {self.course_name}. Направление: {self.direction_course}.'
     
-    def add_module(self, module):
+    def add_module(self, module: str)-> None:
         "Добавление модуля"
         if module not in self.modules:
             self.modules.append(module)
@@ -104,21 +107,21 @@ class Courses:
             print('Модуль уже добавлен!')
 
 
-    def course_started(self, course, user):
+    def course_started(self, course: 'Courses', user:'Users')-> None:
         """Начало курса"""
-        if course.status == 'not started':
+        if course.status == Courses.NOT_STARTED:
             if course in user.personal_course:
-                course.status = 'in progress'
+                course.status = Courses.IN_PROGRESS
                 print(f'\nКурс {course.course_name} начат! Успехов!')
             else:
                 print(f'Нельзя начать курс {course.course_name}, он не выбран вами!')
         else:
             print(f'Курс {course.course_name} уже начат!')
 
-    def course_update(self):
+    def course_update(self)-> None:
         """Обновление курса и прогресс"""
-        if self.status == 'in progress':
-            complete_module = sum(1 for module in self.modules if module.status == 'complete')
+        if self.status == Courses.IN_PROGRESS:
+            complete_module = sum(1 for module in self.modules if module.status == Courses.COMPLETE)
             if len(self.modules) > 0:
                 self.progress = round(complete_module / len(self.modules) * 100)
             else:
@@ -126,32 +129,36 @@ class Courses:
         else:
             print('Начните проходить курс!')
 
-    def complete_course(self, user):
+    def complete_course(self, user: Users)-> None:
         """Завершаем курс"""
         if self.progress == 100:
-            self.status = 'complete'
-            print('Вы завершили курс. Поздравляем!')
+            self.status = Courses.COMPLETE
+            print('Вы завершили курс. Поздравляем!\n')
             user.personal_course.remove(self)
             user.history_complete_course.append(self)
         else:
             print('Курс еще не пройден! Завершите все модули!')
+
             
 class Module:
     "Для управления модулями"
-    def __init__(self, title):
+    NOT_STARTED = 'not started'
+    IN_PROGRESS = 'in progress'
+    COMPLETE = 'complete'
+    def __init__(self, title: str)-> None:
         """Инициализация атрибутов"""
-        self.title = title
-        self.status = 'not started'
-        self.progress = 0
+        self.title: str = title
+        self.status: str = Module.NOT_STARTED
+        self.progress: int = 0
     
-    def start_module(self):
+    def start_module(self)-> None:
         """Начинаем модуль"""
-        self.status = 'in progress'
+        self.status = Module.IN_PROGRESS
         
-    def complete_module(self, course):
+    def complete_module(self, course: 'Courses')-> None:
         """Завершаем модуль"""
-        if self.status == 'in progress':
-            self.status = 'complete'
+        if self.status == Module.IN_PROGRESS:
+            self.status = Module.COMPLETE
             self.progress = 100
             course.course_update()
             print('Поздравляем над завершением модуля!')
@@ -199,18 +206,22 @@ platform.add_course('Курс 3', course3)
 course1.course_started(course1, user1)
 
 module1.start_module()
+
 module1.complete_module(course1)
 user1.show_progress()
+
 module2.start_module()
 module2.complete_module(course1)
+
 user1.show_progress()
 module3.start_module()
+
 module3.complete_module(course1)
 user1.show_progress()
 
-
-
 course1.course_update()
-course1.complete_course(user1)  
+course1.complete_course(user1)
+
+
 
 user1.print_history_complete_courses()
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from remote_platform import Users, Platform, Courses 
+from remote_platform import Users, Platform, Courses, Module
 
 class TestUsers(unittest.TestCase):
     def setUp(self):
@@ -13,6 +13,13 @@ class TestUsers(unittest.TestCase):
         self.course1 = Courses('Основы программирования на Python', 'Programming')
         self.course2 = Courses('Основы программирования на JS', 'Programming')
         self.course3 = Courses('Математика. Продвинутый уровень', 'Educations')
+
+        self.module1 = Module('Модуль 1')
+        self.module2 = Module('Модуль 2')
+        self.module3 = Module('Модуль 3')
+        
+
+         
 
     def test_add_user(self):
         """Проверка что пользователь находится в словаре"""
@@ -32,6 +39,28 @@ class TestUsers(unittest.TestCase):
         self.assertEqual(self.platform.all_users['Пользователь 1'], self.user1)
         self.assertEqual(self.platform.all_users['Пользователь 2'], self.user2)
         self.assertEqual(self.platform.all_users['Пользователь 3'], self.user3)
+
+    def test_status_course(self):
+        """Проверка на вывод правильного курса"""
+        self.user1.select_course(self.course1)
+        self.platform.add_course('Курс 1', self.course1)
+        self.course1.course_started(self.course1, self.user1)
+
+        self.module1.start_module()
+        self.module1.complete_module(self.course1)
+        self.user1.show_progress()
+        self.module2.start_module()
+        self.module2.complete_module(self.course1)
+        self.user1.show_progress()
+        self.module3.start_module()
+        self.module3.complete_module(self.course1)
+        self.user1.show_progress()
+        self.course1.complete_course(self.user1) 
+        self.assertEqual(self.course1.status, self.course1.COMPLETE)
+        
+
+
+        
         
 
 if __name__ == '__main__':
